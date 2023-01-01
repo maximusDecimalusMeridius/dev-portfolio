9d706999bf5c4ff3bfc55939e34fb4002c519208
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-01-01 11:57:09 -0800

    Created initial HTML for gallery card track

---
+++ script.js
@@ -0,0 +1,5 @@
+let _leftButton = document.getElementById("left-button");
+let _rightButton = document.getElementById("right-button");
+
+let _cardTrack = document.getElementById("card-track");
+let _galleryMap = document.getElementById("gallery-map");
